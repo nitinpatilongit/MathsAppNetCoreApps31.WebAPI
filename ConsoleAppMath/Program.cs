31132e0b4939cea21e00e6c68df6690d69ed5568
Nitin <mailfromnitin@gmail.com>
2020-09-07 15:02:31 +0530

    Added dll to nuget.org

---
+++ ConsoleAppMath/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathsAppNetCoreApps31.WebAPI;
+namespace ConsoleAppMath
+{
+    public class Program
+    {
+        public static Utilities objUtilities;
+        static void Main(string[] args)
+        {
+            objUtilities = new Utilities();
+            Console.WriteLine("Hello World!");
+            int a = objUtilities.Add(2, 3);
+
+        }
+    }
+}
---
+++ MathsAppNetCoreApps31.WebAPI/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathsAppNetCoreApps31.WebAPI
+{
+    public class Utilities
+    {
+        public int Add(int a , int b)
+        {
+            return a + b;
+        }
+    }
+}
